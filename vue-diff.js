fc34448d3afe0c2ab7dbd9e95bd9d3bb09daa708
unknown <416212316@qq.com>
2021-12-23 20:58:05 +0800

    left loop

---
+++ vue-diff.js
@@ -0,0 +1,23 @@
+exports.vueDiff = (c1, c2, { mountElement, patch, unmount, move }) => {
+    function isSameVnodeType(n1, n2) {
+        return n1.key === n2.key; //&& n1.type === n2.type;
+    }
+    let i = 0
+    let e1 = c1.length - 1
+    let e2 = c2.length - 1
+    // *1. 从左边往右查找，如果节点可以复用，则继续往右，不能就停止循环
+    while(i <= e1 && i <= e2) { // 通过上面分析我们可以知道循环条件
+        // 取出新老元素
+        const n1 = c1[i]
+        const n2 = c2[i]
+        // 对比是否一样
+        if(isSameVnodeType(n1, n2)) {
+            // 一样就递归调用
+            patch(n1, n2)
+        } else {
+            // 如果不一样就退出循环
+            break
+        }
+        i ++ // 指针往后移动
+    }
+}
