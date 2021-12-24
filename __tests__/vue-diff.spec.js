6f0319284769accca9d0a8b24a73db7533b35e9c
unknown <416212316@qq.com>
2021-12-24 22:37:11 +0800

    fix

--- __tests__/vue-diff.spec.js
+++ __tests__/vue-diff.spec.js
@@ -43,15 +43,37 @@ describe('Vue VNode Diff', () => {
     expect(patch.mock.calls[1][0]).toBe('b')
   })
 
-    it("3. 老节点没了，新节点还有", () => {
+  it('3. 老节点没了，新节点还有', () => {
+    const mountElement = jest.fn()
+    const patch = jest.fn()
+    const unmount = jest.fn()
+    const move = jest.fn()
+    const { vueDiff } = require('../vue-diff')
+    vueDiff(
+      [{ key: 'a' }, { key: 'b' }],
+      [{ key: 'a' }, { key: 'b' }, { key: 'c' }],
+      {
+        mountElement,
+        patch,
+        unmount,
+        move
+      }
+    )
+    expect(patch.mock.calls.length).toBe(2)
+    expect(patch.mock.calls[0][0]).toBe('a')
+    expect(patch.mock.calls[1][0]).toBe('b')
+    expect(mountElement.mock.calls[0][0]).toBe('c')
+  })
+
+  it("4. 老节点还有，新节点没了", () => {
     const mountElement = jest.fn();
     const patch = jest.fn();
     const unmount = jest.fn();
     const move = jest.fn();
     const { vueDiff } = require("../vue-diff");
     vueDiff(
-      [{ key: "a" }, { key: "b" }],
       [{ key: "a" }, { key: "b" }, { key: "c" }],
+      [{ key: "a" }, { key: "b" }],
       {
         mountElement,
         patch,
@@ -59,9 +81,11 @@ describe('Vue VNode Diff', () => {
         move,
       }
     );
+    // 第一次调用次数
     expect(patch.mock.calls.length).toBe(2);
+    // 第一次调用的第一个参数
     expect(patch.mock.calls[0][0]).toBe("a");
     expect(patch.mock.calls[1][0]).toBe("b");
-    expect(mountElement.mock.calls[0][0]).toBe("c");
+    expect(unmount.mock.calls[0][0]).toBe("c");
   });
 })
