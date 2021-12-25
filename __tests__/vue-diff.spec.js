8edce920c8fda1d332681a16a1eb5856efa24e0e
unknown <416212316@qq.com>
2021-12-25 16:30:12 +0800

    fix

--- __tests__/vue-diff.spec.js
+++ __tests__/vue-diff.spec.js
@@ -65,27 +65,73 @@ describe('Vue VNode Diff', () => {
     expect(mountElement.mock.calls[0][0]).toBe('c')
   })
 
-  it("4. 老节点还有，新节点没了", () => {
-    const mountElement = jest.fn();
-    const patch = jest.fn();
-    const unmount = jest.fn();
-    const move = jest.fn();
-    const { vueDiff } = require("../vue-diff");
+  it('4. 老节点还有，新节点没了', () => {
+    const mountElement = jest.fn()
+    const patch = jest.fn()
+    const unmount = jest.fn()
+    const move = jest.fn()
+    const { vueDiff } = require('../vue-diff')
     vueDiff(
-      [{ key: "a" }, { key: "b" }, { key: "c" }],
-      [{ key: "a" }, { key: "b" }],
+      [{ key: 'a' }, { key: 'b' }, { key: 'c' }],
+      [{ key: 'a' }, { key: 'b' }],
       {
         mountElement,
         patch,
         unmount,
-        move,
+        move
       }
-    );
+    )
     // 第一次调用次数
-    expect(patch.mock.calls.length).toBe(2);
+    expect(patch.mock.calls.length).toBe(2)
     // 第一次调用的第一个参数
-    expect(patch.mock.calls[0][0]).toBe("a");
-    expect(patch.mock.calls[1][0]).toBe("b");
-    expect(unmount.mock.calls[0][0]).toBe("c");
-  });
+    expect(patch.mock.calls[0][0]).toBe('a')
+    expect(patch.mock.calls[1][0]).toBe('b')
+    expect(unmount.mock.calls[0][0]).toBe('c')
+  })
+
+  it('5. 删掉老的：在老的里面存在，新的里面不存在', () => {
+    const mountElement = jest.fn()
+    const patch = jest.fn()
+    const unmount = jest.fn()
+    const move = jest.fn()
+    const { vueDiff } = require('../vue-diff')
+    vueDiff(
+      [
+        { key: 'a' },
+        { key: 'b' },
+        { key: 'c' },
+        { key: 'd' },
+        { key: 'e' },
+        { key: 'f' },
+        { key: 'g' }
+      ],
+      [
+        { key: 'a' },
+        { key: 'b' },
+        { key: 'd' },
+        { key: 'c' },
+        { key: 'f' },
+        { key: 'g' }
+      ],
+      {
+        mountElement,
+        patch,
+        unmount,
+        move
+      }
+    )
+    // 第一次调用次数
+    expect(patch.mock.calls.length).toBe(6)
+    // 第一次调用的第一个参数
+    expect(patch.mock.calls[0][0]).toBe('a')
+    expect(patch.mock.calls[1][0]).toBe('b')
+    expect(patch.mock.calls[2][0]).toBe('g')
+    expect(patch.mock.calls[3][0]).toBe('f')
+    expect(patch.mock.calls[4][0]).toBe('c')
+    expect(patch.mock.calls[5][0]).toBe('d')
+    // e在新的里面没有调用删除
+    expect(unmount.mock.calls[0][0]).toBe('e')
+    // e在新的里面没有调用删除
+    expect(unmount.mock.calls.length).toBe(1)
+  })
 })
